1db56b68760f83ee4e8d9faf16e8d8988b74c63b
subzerocr <subzerocr@users.noreply.github.com>
2015-12-06 12:16:20 -0500

    Assets precompile

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,14 @@
-CarrierWave.configure do |config|
+if Rails.env.production?
+	CarrierWave.configure do |config|
 
-  config.fog_credentials = {
-    # Configuration for Amazon S3
-    :provider              => 'AWS',
-    :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
-  config.fog_directory     =  ENV['S3_BUCKET']
-  config.fog_public     = false                                   # optional, defaults to true
-	config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+	  config.fog_credentials = {
+	    # Configuration for Amazon S3
+	    :provider              => 'AWS',
+	    :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+	    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+	  }
+	  config.fog_directory     =  ENV['S3_BUCKET']
+	  config.fog_public     = false                                   # optional, defaults to true
+		config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+	end
 end
