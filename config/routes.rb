97482472b74dc0c06a8b957bc51aba0c0a4f5ca8
subzerocr <subzerocr@users.noreply.github.com>
2016-03-16 16:03:25 -0400

    Remove www

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-
+  constraints subdomain: 'www' do
+  get ':any', to: redirect(subdomain: nil, path: '/%{any}'), any: /.*/
+  end
 
   devise_for :users, controllers: {sessions: "sessions"}
 
