47d125688c5e90d862ba4612a2b6b21be4910d9b
subzerocr <subzerocr@users.noreply.github.com>
2016-04-19 14:05:05 -0400

    Change workouttype batch size

--- app/models/workout_type.rb
+++ app/models/workout_type.rb
@@ -1,4 +1,4 @@
 class WorkoutType < ActiveRecord::Base
-	searchkick text_start: [:name], autocomplete: ['name'], callbacks: :async
+	searchkick text_start: [:name], autocomplete: ['name'], callbacks: :async, batch_size: 200
 
 end
