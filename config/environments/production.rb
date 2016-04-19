082dabd9fcb14ac3391a7dd88e523d55611b66e9
subzerocr <subzerocr@users.noreply.github.com>
2016-04-19 13:28:37 -0400

    Faraday timeout

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,8 @@ Rails.application.configure do
   }
 
   Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['SEARCHBOX_URL']
-  
+  Searchkick.client = Elasticsearch::Client.new(hosts: ["127.0.0.1:9200"], retry_on_failure: true, transport_options: {request: {timeout: 250}})
+
 end
 
 
